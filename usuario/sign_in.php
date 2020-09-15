2bf843c8ee6f3a6f2ed42d2269498c94add78ddd
Vinicius Schulte <schultev2@gmail.com>
2020-09-15 03:51:50 -0300

    Validação de login

--- usuario/sign_in.php
+++ usuario/sign_in.php
@@ -2,41 +2,29 @@
     require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
     require $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php';
 
+    use App\dao\UsuarioDAO;
+    use App\utils\FlashMessages;
+
+    $email =  $_REQUEST['email'];
+    $senha =  $_REQUEST['password'];
+
+    $stmt = UsuarioDAO::getByEmail($email);
+
+    $user = $stmt->fetch(PDO::FETCH_OBJ);
+
+    if($user) {
+        if(password_verify($senha, $user->senha)) {
+            session_start();
+            $_SESSION["logado"] = true ;
+            FlashMessages::setMessage("Usuário: " . $user->email . ", logou com sucesso!");
+            header("Location: /veiculos/");
+        } else {
+            FlashMessages::setMessage("Senha Inválida", "error");
+            header("Location: login.php");
+        }
+
+    } else {
+        FlashMessages::setMessage("Email Inválido", "error");
+        header("Location: login.php");
+    }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <?php include("partials/_head.php") ?>
-    <title>Login</title>
-    <?php include("partials/_javascript_import.php") ?>
-    <link rel="stylesheet" href="sign_in.css">
-</head>
-
-<body>
-    <?php include("partials/_header.php") ?>
-    <div class="cardbox">
-        <div class="Card">
-            <div class="Title">Login</div>
-                <div class="Content">
-                <?php include("partials/_flash_messages.php") ?>
-                    <form action="/usuario/login.php" method="POST">
-                        <div>
-                            <label for="email">Email:</label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="exemplo@exemplo.com">
-                        </div>
-                        <div>
-                            <label for="password" class="mt-2">Senha:</label>
-                            <input type="password" class="form-control" id="password" name="password">
-                        </div>
-                        <div>
-                            <input type="submit" value="Entrar" class="btn btn-success mt-2 float-right">
-                        </div>
-                    </form>
-                </div>
-            </div>
-    </div>
-</body>
-
-</html>
